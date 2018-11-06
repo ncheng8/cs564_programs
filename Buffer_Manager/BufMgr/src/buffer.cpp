bd7562c8799e50b4269278a0a4efc57bb8ef46ea
Nicholas Cheng <nickbaicheng@gmail.com>
2018-11-06 10:53:26 -0600

    Add changes and planning to buffer.cpp

--- Buffer_Manager/BufMgr/src/buffer.cpp
+++ Buffer_Manager/BufMgr/src/buffer.cpp
@@ -36,33 +36,105 @@ BufMgr::BufMgr(std::uint32_t bufs)
 
 
 BufMgr::~BufMgr() {
+	for (FrameId i = 0; i < numBufs; i++) {
+		if (bufDescTable[i].dirty == true) {
+			//TODO flush page
+			//File myFile = bufDescTable[i].file;
+			//writePage bufPool[i]
+		}
+	}
+	delete [] bufDescTable;
+	delete [] bufPool;
+	delete hashTable;
 }
 
 void BufMgr::advanceClock()
 {
-
+	//TODO do it do?
+	if(clockHand >= numBufs - 1){
+		clockHand = 0;
+	}
+	else{
+		clockHand++;
+	}
 }
 
 void BufMgr::allocBuf(FrameId & frame) 
 {
+	//Make a new free frame using the clock algorithm, if necessary
+
+	bool isFull = true;
+	bool allPinned = true;
+	for(int i = 0; i < numBufs; i++){
+		if(bufDescTable[i].valid == false){
+			isFull = false;
+		}
+		if(bufDescTable[i].pinCnt == 0){
+			allPinned = false;
+		}
+	}
+
+	if(allPinned){
+		throw BufferExceededException();
+	}
+
+	if(isFull){
+		//TODO clock algorithm, evict a page if we can
+	}
+
+	//If we kick a valid page out, remove it from the hashtable
+
 }
 
 	
 void BufMgr::readPage(File* file, const PageId pageNo, Page*& page)
 {
+
 }
 
 
 void BufMgr::unPinPage(File* file, const PageId pageNo, const bool dirty) 
 {
+	//TODO
+	FrameId theFrameId = 0;
+	FrameId* fidPtr = &theFrameId;
+	//hashTable->lookup(file, pageNo, fidPtr);
+
+	//If the page is not found...
+	if(theFrameId == 0){
+		return;
+	}
+
+	if(bufDescTable[theFrameId].pinCnt == 0){
+		throw PageNotPinnedException(file->filename, pageNo, theFrameId);
+	}
+
+	bufDescTable[theFrameId].pinCnt--;
+
+	if(dirty){
+		bufDescTable[theFrameId].dirty = dirty;
+	}
 }
 
 void BufMgr::flushFile(const File* file) 
 {
+	//Search through the bufTable for pages belonging to the file
+	for(FrameId i = 0; i < numBufs; i++){
+		if(bufDescTable[i].file == file){
+			if(bufDescTable[i].dirty == true){
+				//Flush the page to disk and set dirty bit to false
+				//TODO
+				badgerdb::Page myPage = bufPool[0];
+				//file->writePage(bufDescTable[i].pageNo, &myPage);
+				bufDescTable[i].dirty = false;
+			}
+		}
+	}
 }
 
 void BufMgr::allocPage(File* file, PageId &pageNo, Page*& page) 
 {
+
 }
 
 void BufMgr::disposePage(File* file, const PageId PageNo)
